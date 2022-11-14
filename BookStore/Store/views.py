307e97efa34c2db7853bb1676f2cb97407c7fad9
spechtigor <specht.igor1984@gmail.com>
2022-11-14 15:46:30 +0100

    add some book create filter search ordering in views

--- BookStore/Store/views.py
+++ BookStore/Store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from Store.models import Book
@@ -9,8 +10,16 @@ from Store.serializers import BooksSerializer, AuthorsSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_fields = ['title', 'description']
+    search_fields = ['title', 'author_id']
+    ordering_fields = ['title', 'author_id']
 
 
 class AuthorViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = AuthorsSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_fields = ['name']
+    search_fields = ['name']
+    ordering_fields = ['name']
